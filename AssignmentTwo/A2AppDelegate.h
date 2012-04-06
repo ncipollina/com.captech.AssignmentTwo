1202c6b40232b70ab2f025126c22a35b5e672cb6
Nick Cipollina <ncipollina@gmail.com>
2012-04-06 13:07:12 -0400

    Initial Commit

---
+++ AssignmentTwo/A2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  A2AppDelegate.h
+//  AssignmentTwo
+//
+//  Created by Nicholas Cipollina on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AssignmentTwo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AssignmentTwo
+//
+//  Created by Nicholas Cipollina on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "A2AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([A2AppDelegate class]));
+    }
+}
